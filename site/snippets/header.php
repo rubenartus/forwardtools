5e4b82cc56fa73770bfe04d236d2655b40d4a58f
Ruben Artus <mail@rubenartus.com>
2023-12-20 13:34:29 +0100

    feat: add tailwind

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,16 +3,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
   <title><?= $page->title() ?> | <?= $site->title() ?></title>
-  <!-- Include Tailwind CSS -->
-  <link href="/path/to/your/tailwind.css" rel="stylesheet">
+
+  <link href="/assets/css/styles.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100 text-gray-800">
   <header class="bg-white shadow">
     <nav class="container mx-auto p-6">
-      <a href="<?= $site->url() ?>" class="text-lg font-bold"><?= $site->title() ?></a>
+      <a href="<?= $site->url() ?>" class="text-lg font-bold<?php if ($page->isActive()): ?> text-indigo-500<?php endif; ?>"><?= $site->title() ?></a>
       <?php foreach ($site->children()->listed() as $item): ?>
-        <a href="<?= $item->url() ?>" class="ml-4"><?= $item->title() ?></a>
+        <a href="<?= $item->url() ?>" class="ml-4<?php if ($item->isActive()): ?> text-indigo-500<?php endif; ?>"><?= $item->title() ?></a>
       <?php endforeach ?>
     </nav>
   </header>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './site/templates/**/*.php',
+    './site/snippets/**/*.php'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
