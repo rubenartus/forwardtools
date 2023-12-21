a2eae0b07f82b7dde27179a7102f39a2dcab7541
Ruben Artus <mail@rubenartus.com>
2023-12-21 10:54:25 +0100

    feat: add pages to panel sidebar

--- site/config/config.php
+++ site/config/config.php
@@ -8,7 +8,61 @@ return [
       'action'  => function () {
         return page('feed');
       }
-    ],
-    // Other routes...
+    ]
+  ],
+  'panel' => [
+    'menu' => [
+      'articles' => [
+        'icon'  => 'pen',
+        'label' => 'Articles',
+        'link'  => 'pages/articles',
+        'current' => function () {
+          $kirby = kirby();
+          $path = $kirby->request()->path()->toString();
+          return Str::contains($path, 'pages/articles');
+        }
+      ],
+      'projects' => [
+        'icon'  => 'bolt',
+        'label' => 'Projects',
+        'link'  => 'pages/projects',
+        'current' => function () {
+          $kirby = kirby();
+          $path = $kirby->request()->path()->toString();
+          return Str::contains($path, 'pages/projects');
+        }
+      ],
+      'questions' => [
+        'icon'  => 'chat',
+        'label' => 'Questions',
+        'link'  => 'pages/questions',
+        'current' => function () {
+          $kirby = kirby();
+          $path = $kirby->request()->path()->toString();
+          return Str::contains($path, 'pages/questions');
+        }
+      ],
+      'links' => [
+        'icon'  => 'heart',
+        'label' => 'Links',
+        'link'  => 'pages/outbound',
+        'current' => function () {
+          $kirby = kirby();
+          $path = $kirby->request()->path()->toString();
+          return Str::contains($path, 'pages/outbound');
+        }
+      ],
+      '-',
+      'site' => [
+        'label' => 'Overview',
+        'current' => function () {
+          $kirby = kirby();
+          $path = $kirby->request()->path()->toString();
+          return Str::contains($path, 'panel/site');
+        }
+      ],
+      'users',
+      'system'
+    ]
   ]
 ];
